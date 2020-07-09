ee75ac0bf69553fd2f529582e98b741ce6c78f96
arnavr23 <arnavvijaywargiya@gmail.com>
2020-07-09 21:58:44 +0530

    added readme and database.sql

---
+++ database.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `java` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `java`;
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: java
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customers` (
+  `account_number` varchar(12) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `pin` int NOT NULL,
+  `balance` float unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`account_number`),
+  UNIQUE KEY `Account Number_UNIQUE` (`account_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES ('123456781234','Arnav Vijaywargiya',1234,12.6),('358897903274','Dimple Gupta',8143,99941),('483920485906','Sasuke Uchiha',5678,50),('86813417817','Nagato',3422,32);
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-09 21:56:24
--- readme.md
+++ readme.md
@@ -1,18 +1,20 @@
 #  **Welcome to ATMSimulation** 23/6/2020
-This is a project created and handled by arnavr23
 
-This is a leisure project intended to simulate an ATM Machine.
 
-It is written in java.
++ This is a project created and handled by arnavr23
 
-It uses Mysql database to store accounts.
++ This is a leisure project intended to simulate an ATM Machine.
 
-It is not yet complete.
++ It is written in java.
 
-*Note: 
-+ In the menu system, I have used bot objects which basically prints instructions, takes input
-and then performs actions according to it
-*
++ It is a maven project.
+
++ It uses Mysql database to store accounts.
+
++ It is not yet complete.
+
+*Note:*
++ *In the menu system, I have used bot objects which basically prints instructions, takes input and then performs actions according to it*
 
 Authors:
 
